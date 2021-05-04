d8f958a81489b0164d146a54d4539a5db3de69fe
Gourab997 <Gourabg998@gmail.com>
2021-05-05 04:41:20 +0600

    login register

---
+++ frontend/tours_travel/src/employee/UpdatePackage.js
@@ -0,0 +1,10 @@
+import Header from "./Header";
+function UpdatePackage(){
+    return(
+        <div>
+            <Header/>
+            <h1>Update Package</h1>
+        </div>
+    )
+}
+export default UpdatePackage
